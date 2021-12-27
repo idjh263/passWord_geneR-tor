4ba1234783b2f854769b608bdd08aa36300f18fd
Irene Hipolito <irenehipolito@Irenes-MacBook-Air.local>
2021-12-26 19:48:47 -0500

    working js and updated README

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Password generator project using Javascript
 #Purpose: we were asked to built a password generator using Javascript. The HTML and CSS were already given. Our task is to just built the JS script. 
 The program initialize when the generate password button is click. 
 The program will ask the length of the password, if it has uppercase, lowercase, number and symbols. The password is limited to 5 to 128 characters and should at least have one character. 
+if any of the above condition were not met, need to refresh the page. 
 
--UNFINISHED PROJECT
+
 
 #Built With:
  - HTML
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,67 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
 
 
-var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z' ]; 
-var lowerCase = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',  's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbols = ["!", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "?", "/", "-", "+"];
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split(""); 
+  var lowerCase = "abcdefghiklmnopqrsttuvwxtz" .split("");
+  var numbers = "0123456789" .split ("");
+  var symbols =  "*&^/\%$#@!?[]><{}" .split("");
 
 
-generateBtn.addEventListener("click", writePassword);
-  
+  function generatePassword() {
+  var characterGen = []; //a
+  var randomPassword = ""; //pw
 
-  function writePassword() {
-   
-  var passwordLength = parseInt(prompt("How many password characters do you need, from 5 to 128?"));
-  if (!passwordLength || passwordLength <= 4 || passwordLength > 128) {
-    return prompt = parseInt("Please enter value between 5 to 128");
-  }else {
-    withUpper = confirm("Do you need an uppercase?");
-    withLower = confirm("Do you need a lowercase?");
-    withNumbers = confirm("Do you need a number?");
-    withSymbols = confirm("Do you need a symbol?");
+
+
+  var refreshPage = function() {
+    location.reload();
   };
-  if(!withLower && !withUpper && !withNumbers && !withSymbols) {
-  genPassword = alert("please enter at least one character"); 
 
+  var passwordLength = window.prompt("How many password characters do you need from 8 to 128?");
+  passwordLength = Number(passwordLength); 
+  if (passwordLength < 8 || passwordLength > 128 || NaN) {
+     window.prompt = ("Please enter value between 8 to 128");
+     randomPassword = "please try again, enter value between 8 to 128 and refresh page";
+  
+  }else { 
+    var withLowerCase = confirm("do you need lower case?");
+    if(withLowerCase) {
+      characterGen = characterGen.concat(lowerCase);
+    }
+    var withUpperCase = confirm("do you need upper case?");
+    if(withUpperCase) {
+      characterGen = characterGen.concat(upperCase);
+    }
+    var withNumbers = confirm("do you need numbers?");
+    if(withNumbers) {
+      characterGen = characterGen.concat(numbers);
+    }
+    var withSymbols = confirm("do you need special characters?");
+    if(withSymbols) {
+      characterGen = characterGen.concat(symbols);
+    }
+    for (var i = 0; i < passwordLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * characterGen.length);
+    randomPassword += characterGen[randomCharacter];
+    }
+    if (characterGen.length == 0) {
+      alert("please enter at least one character type"); 
+      randomPassword = "please try again, needs at least 1 character"
   }
-  else if (withLower && withUpper && withNumbers && withSymbols) {
-  genPassword = symbols.concat(upperCase, numbers,symbols);
-  }else if (withSymbols && withNumbers && withUpper) {
-  genPassword = symbols.concat(numbers, upperCase);
   }
-else if (withSymbols && withNumbers && withLower) {
-  genPassword = symbols.concat(numbers, lowerCase);
-  }
-else if (withSymbols && withLower && withUpper) {
-  genPassword = symbols.concat(lowerCase, upperCase);
-  }
-else if (withNumbers && withLower && withUpper) {
-  genPassword = numbers.concat(lowerCase, upperCase);
-}
-else if (withSymbols && withNumbers) {
-  genPassword = symbols.concat(numbers);
-
-} else if (withSymbols && withLower) {
-  genPassword = symbols.concat(lowerCase);
-
-} else if (withSymbols && withUpper) {
-  genPassword = symbols.concat(upperCase);
-}
-else if (withLower && withNumbers) {
-  genPassword = lowerCase.concat(numbers);
-
-} else if (withLower && withUpper) {
-  genPassword = lowerCase.concat(upperCase);
-
-} else if (withNumbers && withUpper) {
-  genPassword = numbers.concat(upperCase);
-}
-else if (withSymbols) {
-    genPassword = symbols;
-}
-else if (withNumbers) {
-    genPassword = numbers;
-}
-else if (withLower) {
-    genPassword = lowerCase;
-}
-else if (withUpper) {
-    genPassword = upperCase;
-
+  return randomPassword; 
 };
 
-var password = [];
-for (var i = 0; i < passwordLength; i ++) {
-  var genPass = genPassword[Math.floor(Math.random() * genPassword.length)];
-  password.push(genPass);
-}
+ function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
  
-
+  passwordText.value = password; 
 }
-  
+
+generateBtn.addEventListener("click", writePassword);
+
 
     
 
